a71379af9dee77c3fb0b7066c60db03b855f4499
TranTatViet21020132 <21020132@vnu.edu.vn>
2024-08-07 16:41:57 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Em Trang ngu ngok
         </a>
       </header>
     </div>
